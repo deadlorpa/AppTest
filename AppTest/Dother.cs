8459e372f3b7942ca19bee07b74d15cd981a65ce
deadlorpa <dealorpa@gmail.com>
2021-10-07 15:15:09 +0700

    Добавьте файлы проекта.

---
+++ AppTest/Dother.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class Dother : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_second);
+            Button buttonBACK = FindViewById<Button>(Resource.Id.buttonBACK);
+            EditText editTextBACK = FindViewById<EditText>(Resource.Id.editTextBACK);
+            editTextBACK.Text = Intent.GetStringExtra("info");
+            buttonBACK.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                intent.PutExtra("infoBack", editTextBACK.Text.ToString()+" #ядочь");
+                StartActivity(intent);
+            };
+        }
+    }
+}
---
+++ AppTest/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Content;
+
+namespace AppTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Button buttonGO = FindViewById<Button>(Resource.Id.buttonGO);
+            EditText editTextGO = FindViewById<EditText>(Resource.Id.editTextGO);
+            buttonGO.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(Dother));
+                intent.PutExtra("info", editTextGO.Text.ToString());
+                StartActivity(intent);
+            };
+            editTextGO.Text = Intent.GetStringExtra("infoBack");
+        }
+
+    }
+}
